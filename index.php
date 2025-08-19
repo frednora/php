57f9baa8cfc390ce25baa4bba32065ea88f68aec
Fred Nora <frednora@outlook.com>
2025-08-19 18:43:53 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <?php 
+        // magic thingy
+        //$magic = __FILE__;
+        $magic = __LINE__;
+        $var01 = "<br><i>Hello World!</i>";
+        $var02 = "<br>The thing is: $magic";
+	echo ($var01);
+	echo ($var02);
+    ?>
+</html>
+
